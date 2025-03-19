2cde9c68788cb7abae87bbcc27d90a6cd98aaecb
Ronald-Wesh <ronaldwaweru8@gmail.com>
2025-03-19 17:21:05 +0300

    Add files via upload

--- week 3 Assignment.py
+++ week 3 Assignment.py
@@ -3,10 +3,11 @@ def calculate_discount(price, discount_percent):
         discount_amount = (discount_percent * price) / 100
         final_price=price-discount_amount
         print(f"Price is {final_price}")
-        return final_price
+
     else :
         print(f"Price is {price}")
-        return price
+
 price=float(input("Enter price :"))
 discount_percent=float(input("Enter discount:"))
 calculate_discount(price, discount_percent)
+print(f"Original price is : {price} ")
